b1f8d0f91ddd120ac2eea6a2f2e61158d29c4f92
Baxter Pearson <baxterrpearson@gmail.com>
2025-11-06 18:27:11 +0000

    Text added to news widgets section

--- src/components/WebWidgets.tsx
+++ src/components/WebWidgets.tsx
@@ -4,6 +4,9 @@ const WebWidgets = () => {
       <div className="container mx-auto px-4 md:px-6">
         <div className="text-center mb-10">
           <h2 className="text-3xl md:text-4xl font-bold text-white">News Widgets</h2>
+          <p className="mt-4 text-xl text-gray-400 max-w-2xl mx-auto">
+            Here are my Formula 1, Cryptocurrency, and Premier League Football news widgets, which can be embedded in any website. The news sources are updated 24 times per day.
+          </p>
         </div>
 
         <div className="grid grid-cols-1 xl:grid-cols-3 md:grid-cols-1 grid-rows-1 place-items-center gap-2 p-8" >
